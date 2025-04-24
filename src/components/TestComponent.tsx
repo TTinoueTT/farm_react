5ceb157137cb284911b59628c2b636f6f528fd3b
t_inoue <t.artwork.ceramica@gmail.com>
2025-04-24 16:26:35 +0900

    sample: useState の応用
    - ステート関数を2回呼び出す処理でステート変数を更新する処理を作成

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -2,16 +2,18 @@ import { useState } from "react"
 
 export const TestComponent = () => {
 	const [count, setCount] = useState(0)
+	// React では、count と setCount のことをステート変数とステート関数と呼びます
+	const onClickChange = () => {
+		setCount((count) => count + 1)
+		setCount((count) => count * 2)
+	}
 
 	return (
 		<div>
 			<h3>TestComponent</h3>
 			<p>Count: {count}</p>
-			<button
-				type="button"
-				onClick={() => setCount((count) => count + 1)}
-			>
-				Increment
+			<button type="button" onClick={onClickChange}>
+				1 足した後に 2 倍する
 			</button>
 		</div>
 	)
